43b3f637f1a30cd140e8261505684f3b612061de
Mart van Buren <mart@fractic.io>
2024-07-13 16:39:43 +0900

    Add tests for schema::Timestamp.

--- src/schema/timestamp.rs
+++ src/schema/timestamp.rs
@@ -23,3 +23,57 @@ impl Timestamp {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Utc;
+
+    #[test]
+    fn test_now() {
+        let timestamp = Timestamp::now();
+        let now = Utc::now();
+
+        // Check if the timestamp is close enough to now.
+        assert!(timestamp.seconds <= now.timestamp());
+        assert!(timestamp.nanos <= now.timestamp_subsec_nanos());
+    }
+
+    #[test]
+    fn test_from_utc_datetime() {
+        let dt = Utc::now();
+        let timestamp = Timestamp::from_utc_datetime(&dt);
+
+        assert_eq!(timestamp.seconds, dt.timestamp());
+        assert_eq!(timestamp.nanos, dt.timestamp_subsec_nanos());
+    }
+
+    #[test]
+    fn test_to_utc_datetime() {
+        let dt = Utc::now();
+        let timestamp = Timestamp::from_utc_datetime(&dt);
+        let result = timestamp.to_utc_datetime();
+
+        match result {
+            Ok(converted_dt) => {
+                assert_eq!(converted_dt.timestamp(), dt.timestamp());
+                assert_eq!(
+                    converted_dt.timestamp_subsec_nanos(),
+                    dt.timestamp_subsec_nanos()
+                );
+            }
+            Err(_) => panic!("Conversion failed"),
+        }
+    }
+
+    #[test]
+    fn test_to_utc_datetime_error() {
+        let timestamp = Timestamp {
+            seconds: 0,
+            nanos: 2_000_000_000, // Invalid nanoseconds value
+        };
+        let result = timestamp.to_utc_datetime();
+
+        assert!(result.is_err());
+    }
+}

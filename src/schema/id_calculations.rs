b0aa930332099c6f2f09f7e9f0beee47d4de411e
Mart van Buren <mart@fractic.io>
2024-07-13 16:55:27 +0900

    Add tests for schema::id_calculations.

--- src/schema/id_calculations.rs
+++ src/schema/id_calculations.rs
@@ -64,3 +64,87 @@ pub fn get_pk_sk_from_map(map: &DynamoMap) -> Result<(&str, &str), GenericServer
             .map_err(|_| gen_err())?,
     ))
 }
+
+#[cfg(test)]
+mod tests {
+    use aws_sdk_dynamodb::types::AttributeValue;
+    use fractic_core::collection;
+
+    use super::*;
+
+    #[test]
+    fn test_base62_encode() {
+        let encoded = _base62_encode(1234567890);
+        assert_eq!(encoded.len(), NUM_CHARACTERS_TO_GENERATE);
+        // As the length is fixed, padding may occur; ensure the encoded string meets this condition.
+        assert!(encoded.chars().all(|c| ALPHABET.contains(&(c as u8))));
+    }
+
+    #[test]
+    fn test_generate_uuid() {
+        let uuid = generate_uuid();
+        assert_eq!(uuid.len(), NUM_CHARACTERS_TO_GENERATE);
+        // Ensure the UUID is base62 encoded.
+        assert!(uuid.chars().all(|c| ALPHABET.contains(&(c as u8))));
+    }
+
+    #[test]
+    fn test_get_object_type() {
+        assert_eq!(get_object_type("USER#123", "ROOT"), "USER");
+        assert_eq!(get_object_type("ROOT", "ORDER#456"), "ORDER");
+        assert_eq!(get_object_type("ROOT", "ORDER#456#ITEM#789"), "ITEM");
+        assert_eq!(get_object_type("ORDER#123", "ITEM#789"), "ITEM");
+        assert_eq!(get_object_type("USER#123", "ORDER#456#ITEM#910"), "ITEM");
+    }
+
+    #[test]
+    fn test_get_last_id_label() {
+        assert_eq!(get_last_id_label("USER#123"), "USER");
+        assert_eq!(get_last_id_label("USER#123#ORDER#456"), "ORDER");
+        assert_eq!(get_last_id_label("ROOT"), "ROOT");
+        assert_eq!(get_last_id_label("USER#123#ORDER#456#ITEM#789"), "ITEM");
+    }
+
+    #[test]
+    fn test_get_pk_sk_from_string() {
+        // Valid:
+        match get_pk_sk_from_string("USER#123#CHILD#456|ORDER#456#ITEM#abc1234DEF") {
+            Ok((pk, sk)) => {
+                assert_eq!(pk, "USER#123#CHILD#456");
+                assert_eq!(sk, "ORDER#456#ITEM#abc1234DEF");
+            }
+            Err(e) => panic!("Expected Ok, got Err: {}", e),
+        }
+
+        // Invalid:
+        match get_pk_sk_from_string("invalid_format_string") {
+            Ok(_) => panic!("Expected Err, got Ok"),
+            Err(e) => assert!(e.to_string().contains("not in format pk|sk")),
+        }
+    }
+
+    #[test]
+    fn test_get_pk_sk_from_map() {
+        // With required fields:
+        let map = collection!(
+            "pk".to_string() => AttributeValue::S("USER#123#CHILD#456".to_string()),
+            "sk".to_string() => AttributeValue::S("ORDER#456#ITEM#abc1234DEF".to_string())
+        );
+        match get_pk_sk_from_map(&map) {
+            Ok((pk, sk)) => {
+                assert_eq!(pk, "USER#123#CHILD#456");
+                assert_eq!(sk, "ORDER#456#ITEM#abc1234DEF");
+            }
+            Err(e) => panic!("Expected Ok, got Err: {}", e),
+        }
+
+        // Without required fields:
+        let invalid_map = collection!(
+            "pk".to_string() => AttributeValue::S("USER#123#CHILD#456".to_string()),
+        );
+        match get_pk_sk_from_map(&invalid_map) {
+            Ok(_) => panic!("Expected Err, got Ok"),
+            Err(e) => assert!(e.to_string().contains("did not contain pk/sk fields")),
+        }
+    }
+}

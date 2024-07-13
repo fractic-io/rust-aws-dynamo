dc44fd3edbb7d28cf0371008ab2b22acfa887ff5
Mart van Buren <mart@fractic.io>
2024-07-13 16:34:59 +0900

    Add tests for schema::PkSk.

--- src/schema/pk_sk.rs
+++ src/schema/pk_sk.rs
@@ -53,3 +53,55 @@ impl<'de> Deserialize<'de> for PkSk {
         })
     }
 }
+
+// Tests.
+// --------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn test_root() {
+        let pksk = PkSk::root();
+        assert_eq!(pksk.pk, "ROOT");
+        assert_eq!(pksk.sk, "ROOT");
+    }
+
+    #[test]
+    fn test_from_string_valid() {
+        let json_str = r#""test_pk|test_sk""#;
+        // Using from_string:
+        let pksk = PkSk::from_string(json_str).unwrap();
+        assert_eq!(pksk.pk, "test_pk");
+        assert_eq!(pksk.sk, "test_sk");
+        // Using Deserialize:
+        let pksk: PkSk = serde_json::from_str(json_str).unwrap();
+        assert_eq!(pksk.pk, "test_pk");
+        assert_eq!(pksk.sk, "test_sk");
+    }
+
+    #[test]
+    fn test_from_string_invalid() {
+        // Using from_string:
+        let json_str = r#"invalid_format"#;
+        assert!(PkSk::from_string(json_str).is_err());
+        // Using Deserialize:
+        let json_str = r#""invalid_format""#;
+        assert!(serde_json::from_str::<PkSk>(json_str).is_err());
+    }
+
+    #[test]
+    fn test_to_string() {
+        let pksk = PkSk {
+            pk: "test_pk".to_string(),
+            sk: "test_sk".to_string(),
+        };
+        // Using display:
+        assert_eq!(format!("{}", pksk), "test_pk|test_sk");
+        // Using Serialize:
+        let serialized = serde_json::to_string(&pksk).unwrap();
+        assert_eq!(serialized, r#""test_pk|test_sk""#);
+    }
+}

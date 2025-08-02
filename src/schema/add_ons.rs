a34b166a747637cbee3707a1ba15e5e24fa07243
Mart van Buren <mart@fractic.io>
2025-08-02 00:15:30 +0000

    Auto-import required traits in macros.

--- src/schema/add_ons.rs
+++ src/schema/add_ons.rs
@@ -9,6 +9,8 @@ macro_rules! with_impl_display {
     ($type:ident) => {
         impl std::fmt::Display for $type {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                use $crate::schema::DynamoObject as _;
+                use $crate::schema::MaybeCommittedDynamoObject as _;
                 write!(f, "{}", self.data())
             }
         }
